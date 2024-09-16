a055f9f4a299efa9c957a03ac4a10e7550bf572f
AhmedFalahALharbi <ahmedfalahalharbi@gmail.com>
2024-09-16 14:19:31 +0300

    create file

---
+++ main.js
@@ -0,0 +1,55 @@
+let name1 = document.getElementById('name')
+let imgsrc = document.getElementById('imgsrc')
+let text = document.getElementById('text')
+let btn = document.getElementById('btn')
+
+btn.addEventListener('click' , ()=> {
+    fetch('https://66e7e6bfb17821a9d9da7097.mockapi.io/image', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name:name1.value,
+            image: imgsrc.value
+        }     
+        ),
+      })
+        .then(response => response.json())
+        .then(data => {
+            text.textContent = "Img Uploaded";
+        })
+})
+
+let container = document.getElementById('container')
+
+fetch('https://66e7e6bfb17821a9d9da7097.mockapi.io/image')
+  .then(response => response.json())
+  .then(data => {
+       
+    let logoName = data.map(a => a.name);
+    let logoImg = data.map(a => a.image);
+    logoName.forEach((element , index) => {
+        let nameContainer = document.createElement('p')
+        let imgContainer = document.createElement('img')
+        let card = document.createElement('div')
+        nameContainer.classList.add('nameContainer')
+        imgContainer.classList.add('imgContainer')
+        card.classList.add('card')
+        nameContainer.textContent = logoName[index];
+        imgContainer.setAttribute('src',logoImg[index] )
+
+        container.appendChild(card)
+        card.appendChild(nameContainer)
+        card.appendChild(imgContainer)
+    });
+})
+deleteBtn.addEventListener('click', () => {
+    fetch(`https://66e7e6bfb17821a9d9da7097.mockapi.io/image/${item.id}`, {
+        method: 'DELETE',
+    })
+        .then(response => response.json())
+        .then(() => {
+            card.remove(); // Remove the card from the DOM after deletion
+        });
+});
